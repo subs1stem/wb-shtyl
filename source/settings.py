4f0096961e9dc8b0229dfb024b32c7427836b44f
subs1stem <subs1stem@yandex.ru>
2021-12-29 17:06:07 +0300

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -2,7 +2,7 @@ DEVICE_NAME = 'ШТИЛЬ'
 SNMP_DEVICE_ADDRESS = ''
 SNMP_COMMUNITY = b''
 
-MQTT_BROKER_IP = '192.168.13.131'
+MQTT_BROKER_IP = 'localhost'
 ROOT_MQTT_TOPIC = '/devices/shtyl'
 
 POLLING_INTERVAL = 10  # secs
